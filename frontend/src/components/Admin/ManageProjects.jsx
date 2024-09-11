6f25f6f3fb061697bc5c2d4e5729490d58b405d2
georges <118487336+Georgesdm@users.noreply.github.com>
2024-09-11 21:36:14 +0200

    feat: Add ManageProjects and Projects components
    This commit adds two new components: ManageProjects and Projects.
    
    The ManageProjects component allows users to manage projects by adding new projects with image upload and deleting existing projects. It also fetches existing projects from the server and displays them in a list.
    
    The Projects component displays a grid of projects fetched from the server. Each project card includes an image, title, description, and the languages used in the project.
    
    These components are essential for managing and displaying projects in the application.

---
+++ frontend/src/components/Admin/ManageProjects.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+
+const ManageProjects = () => {
+  const [projects, setProjects] = useState([]);
+  const [newProject, setNewProject] = useState({
+    title: "",
+    description: "",
+    languages: "",
+    image: null,
+  });
+
+  // Récupérer les projets existants
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/api/projects", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des projets :", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  // Gérer les changements dans les inputs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProject({ ...newProject, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    setNewProject({ ...newProject, image: e.target.files[0] });
+  };
+
+  // Ajouter un nouveau projet avec l'upload d'image
+  const addProject = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", newProject.title);
+    formData.append("description", newProject.description);
+    formData.append("languages", newProject.languages);
+    formData.append("image", newProject.image);
+
+    const response = await fetch("http://localhost:4000/api/projects", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: formData, // Envoyer le projet et l'image
+    });
+
+    const data = await response.json();
+    setProjects([...projects, data.project]); // Ajouter le nouveau projet à la liste des projets
+    setNewProject({ title: "", description: "", languages: "", image: null }); // Réinitialiser le formulaire
+  };
+
+  const deleteProject = async (projectId) => {
+    try {
+      await fetch(`http://localhost:4000/api/projects/${projectId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      // Mettre à jour l'état pour retirer le projet supprimé
+      setProjects(projects.filter((project) => project._id !== projectId));
+    } catch (error) {
+      console.error("Erreur lors de la suppression du projet:", error);
+    }
+  };
+
+  return (
+    <div className="manage-projects">
+      <h2>Gérer les Projets</h2>
+
+      <form onSubmit={addProject}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Titre"
+          value={newProject.title}
+          onChange={handleInputChange}
+          required
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={newProject.description}
+          onChange={handleInputChange}
+          required
+        />
+        <input
+          type="text"
+          name="languages"
+          placeholder="Langages utilisés"
+          value={newProject.languages}
+          onChange={handleInputChange}
+          required
+        />
+        <input type="file" name="image" onChange={handleImageChange} required />
+        <button type="submit">Ajouter le projet</button>
+      </form>
+
+      <h2>Liste des projets</h2>
+      <ul>
+        {projects.map((project) => (
+          <li key={project._id}>
+            Titre: {project.title} Ajouté le: {project.dateCreated}
+            <button onClick={() => deleteProject(project._id)}>
+              Supprimer
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ManageProjects;
---
+++ frontend/src/components/Projects/Projects.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Projects.scss";
+import { useState, useEffect } from "react";
+
+const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/api/projects");
+        const data = await response.json();
+        setProjects(data); // Mettre à jour l'état avec les projets récupérés
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <div className="projects">
+      <h2>Mes derniers Projets</h2>
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <div className="project-card" key={project._id}>
+            <img
+              src={`http://localhost:4000${project.imageUrl}`}
+              alt={project.title}
+            />
+            <div className="project-tech">
+              {project.languages.map((language, index) => (
+                <span key={index} className="tech-badge">
+                  {language}
+                </span>
+              ))}
+            </div>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
