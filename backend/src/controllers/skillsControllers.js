330b22e565910d8a6e5697ff079074e094a670bb
georges <118487336+Georgesdm@users.noreply.github.com>
2024-09-12 12:01:00 +0200

    refactor: Simplify skillsControllers.js and skills.js

--- backend/src/controllers/skillsControllers.js
+++ backend/src/controllers/skillsControllers.js
@@ -1,73 +1,78 @@
-const Skill = require('../models/skills');
-const fs = require('fs');
-const path = require('path');
+const Skill = require("../models/skills");
+const fs = require("fs");
+const path = require("path");
 
 exports.getAllSkills = async (req, res) => {
   try {
     const skills = await Skill.find();
     res.status(200).json(skills);
   } catch (error) {
-    console.error('Erreur lors de la récupération des compétences:', error);
-    res.status(500).json({ message: 'Erreur interne du serveur' });
+    console.error("Erreur lors de la récupération des compétences:", error);
+    res.status(500).json({ message: "Erreur interne du serveur" });
   }
 };
 
 exports.createSkill = async (req, res) => {
-    const { name, level, description } = req.body;
+  const { name, icon, color } = req.body;
 
-    if (!name || !level || !description) {
-        return res.status(400).json({ message: 'Tous les champs sont requis.' });
-    }
+  if (!name || !icon || !color) {
+    return res.status(400).json({ message: "Tous les champs sont requis." });
+  }
 
-    try {
-        const newSkill = new Skill({ name, level, description });
-        await newSkill.save();
-        res.status(201).json({ message: 'Compétence créée avec succès', skill: newSkill });
-    } catch (error) {
-        console.error('Erreur lors de la création de la compétence:', error);
-        res.status(500).json({ message: 'Erreur interne du serveur' });
-    }
+  try {
+    const newSkill = new Skill({ name, icon, color });
+    await newSkill.save();
+    res
+      .status(201)
+      .json({ message: "Compétence créée avec succès", skill: newSkill });
+  } catch (error) {
+    console.error("Erreur lors de la création de la compétence:", error);
+    res.status(500).json({ message: "Erreur interne du serveur" });
+  }
 };
 
 exports.updateSkill = async (req, res) => {
-    const { id } = req.params;
-    const { name, level, description } = req.body;
-    
-    if (!name || !level) {
-        return res.status(400).json({ message: 'Nom et niveau de compétence sont requis.' });
-    }
-    
-    try {
-        const skill = await Skill.findByIdAndUpdate(id, { name, level, description }, { new: true });
-        res.status(200).json(skill);
-    } catch (error) {
-        console.error('Erreur lors de la mise à jour de la compétence:', error);
-        res.status(500).json({ message: 'Erreur interne du serveur' });
-    }
+  const { id } = req.params;
+  const { name, icon, color } = req.body;
+
+  if (!name || !icon || !color) {
+    return res.status(400).json({ message: "Tous les champs sont requis." });
+  }
+
+  try {
+    const updatedSkill = await Skill.findByIdAndUpdate(
+      id,
+      { name, icon, color },
+      { new: true }
+    );
+    res.status(200).json(updatedSkill);
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour de la compétence:", error);
+    res.status(500).json({ message: "Erreur interne du serveur" });
+  }
 };
 
 exports.deleteSkill = async (req, res) => {
-    const { id } = req.params;
-    
-    try {
-        await Skill.findByIdAndDelete(id);
-        res.status(204).end();
-    } catch (error) {
-        console.error('Erreur lors de la suppression de la compétence:', error);
-        res.status(500).json({ message: 'Erreur interne du serveur' });
-    }
+  const { id } = req.params;
+
+  try {
+    await Skill.findByIdAndDelete(id);
+    res.status(204).end();
+  } catch (error) {
+    console.error("Erreur lors de la suppression de la compétence:", error);
+    res.status(500).json({ message: "Erreur interne du serveur" });
+  }
 };
 
-
 exports.getOneSkill = async (req, res) => {
-    try {
-        const skill = await Skill.findById(req.params.id);
-        if (!skill) {
-            return res.status(404).json({ message: 'Skill non trouvé' });
-        }
-        res.status(200).json(skill);
-    } catch (error) {
-        console.error('Erreur lors de la récupération du skill:', error);
-        res.status(500).json({ message: 'Erreur interne du serveur' });
+  try {
+    const skill = await Skill.findById(req.params.id);
+    if (!skill) {
+      return res.status(404).json({ message: "Skill non trouvé" });
     }
+    res.status(200).json(skill);
+  } catch (error) {
+    console.error("Erreur lors de la récupération du skill:", error);
+    res.status(500).json({ message: "Erreur interne du serveur" });
+  }
 };
--- backend/src/models/skills.js
+++ backend/src/models/skills.js
@@ -1,21 +1,24 @@
-// models/skill.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const skillSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    level: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String, // Le nom de l'icône FontAwesome
+    required: true,
+  },
+  color: {
+    type: String, // Code couleur hexadécimal
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Skill = mongoose.model('Skill', skillSchema);
+const Skill = mongoose.model("Skill", skillSchema);
 
 module.exports = Skill;
